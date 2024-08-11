e25c84b55fb8a031f8e49cf4c83d61134e93111b
AvaRu01 <avaru01@abv.bg>
2024-08-11 14:06:28 +0300

    Add

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question:"What does the setState method in React do?",
+        option1: "Directly modifies the component's state.",
+        option2: "Schedules an update to a component's state object.",
+        option3: "Forces the component to re-render without updating the state.",
+        option4: "Deletes the component's state object.",
+        ans: 2
+    },
+    {
+        question:"Which of the following is used to pass data from a parent component to a child component in React?",
+        option1: "undefined",
+        option2: "number",
+        option3: "NaN",
+        option4: "object",
+        ans: 3
+    },
+    {
+        question:"In JavaScript, what will the expression typeof NaN return?",
+        option1: "State",
+        option2: "Context",
+        option3: "Props",
+        option4: "Hooks",
+        ans: 2
+    },
+    {
+        question:"Which method is used to remove the first element from an array in JavaScript?",
+        option1: "pop()",
+        option2: "push()",
+        option3: "shift()",
+        option4: "unshift()",
+        ans: 3
+    },
+    {
+        question:"What is the purpose of useEffect hook in React?",
+        option1: "To manage the component state.",
+        option2: "To handle form submissions.",
+        option3: "To perform side effects in function components.",
+        option4: "To directly manipulate the DOM.",
+        ans: 3
+    }
+]
